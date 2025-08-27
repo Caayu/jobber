5f2d599845d1ee388b2a3316b7e0cb7dd45f1f50
Caio Henrique <henrycay@outlook.com>
2025-08-27 17:26:57 -0300

    GraphQL & NX Libs

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -0,0 +1,14 @@
+import { Query, Resolver } from '@nestjs/graphql';
+
+import { User } from './models/user.model';
+import { UsersService } from './users.service';
+
+@Resolver(() => User)
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) { }
+
+  @Query(() => [User], { name: 'users' })
+  async getUsers() {
+
+  }
+}
