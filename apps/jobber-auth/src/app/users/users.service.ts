2a85b073f64750bf979aeabf2e9006df99af46a4
Caio Henrique <henrycay@outlook.com>
2025-08-28 09:20:24 -0300

    Users Mutation & Query

--- apps/jobber-auth/src/app/users/users.service.ts
+++ apps/jobber-auth/src/app/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma-clients/jobber-auth';
+import { hash } from 'bcryptjs';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) { }
+
+  async createUser(data: Prisma.UserCreateInput) {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: await hash(data.password, 10)
+      }
+    });
+  }
+
+  async getUsers() {
+    return this.prismaService.user.findMany();
+  }
+}
