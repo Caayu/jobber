36de5b646b79b72775e5d6aedbb42c76ae9b1273
Caio Henrique <henrycay@outlook.com>
2025-08-28 09:41:42 -0300

    Config Module

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
@@ -7,6 +8,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
